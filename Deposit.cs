7b24deab6641dcfc5f2f3d8bdf150cc740d1cda9
SefikaBoz <sefikaboz12@gmail.com>
2017-05-17 21:39:47 +0300

    Create Deposit.cs

---
+++ Deposit.cs
@@ -0,0 +1,34 @@
+# 17-05-2017---4
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMsdn
+{
+
+    class Deposit
+    {
+        public virtual void WithdrawMoney()
+        {
+            Console.WriteLine("WithdrawMoney options");
+        }
+    }
+    class Transfer : Deposit
+    {
+        public override void WithdrawMoney()
+        {
+            Console.WriteLine("WithdrawMoney options with tranfer..");
+        }
+    }
+    class EFT : Deposit
+    {
+        //public override void WithdrawMoney()
+        //{
+        //    Console.WriteLine("WithdrawMoney options with EFT..");
+        //}
+    }
+        
+}
